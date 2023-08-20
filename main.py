1cb0f5f53d111fed8678cf2545de1b3a4f2f84c8
Nurasyl <mozamb1que.top@gmail.com>
2023-08-20 20:26:24 +0700

    ''

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import http.server
-import os
-import threading
 import urllib.parse
-import urllib.request
-import json
 import queue
+import threading
 import time
+import json
 from functools import reduce
+import urllib.request
+import os
 
 # Глобальные очереди для хранения задач сортировки и результатов
 sorting_queue = queue.Queue()
@@ -17,27 +17,29 @@ sorting_data = {}
 sorting_data_lock = threading.Lock()
 
 
+def merge(left, right):
+    merged = []
+    left_idx, right_idx = 0, 0
+
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] < right[right_idx]:
+            merged.append(left[left_idx])
+            left_idx += 1
+        else:
+            merged.append(right[right_idx])
+            right_idx += 1
+
+    merged.extend(left[left_idx:])
+    merged.extend(right[right_idx:])
+    return merged
+
+
 # Функция для выполнения сортировки слиянием
 def merge_sort(numbers):
     if len(numbers) <= 1:
         return numbers
 
     # Функция для слияния двух отсортированных массивов
-    def merge(left, right):
-        merged = []
-        left_idx, right_idx = 0, 0
-
-        while left_idx < len(left) and right_idx < len(right):
-            if left[left_idx] < right[right_idx]:
-                merged.append(left[left_idx])
-                left_idx += 1
-            else:
-                merged.append(right[right_idx])
-                right_idx += 1
-
-        merged.extend(left[left_idx:])
-        merged.extend(right[right_idx:])
-        return merged
 
     # Разделяем массив на две части и рекурсивно сортируем каждую из них
     mid = len(numbers) // 2
@@ -48,69 +50,73 @@ def merge_sort(numbers):
     return reduce(merge, [left_half, right_half])
 
 
-# Функция для обработки запроса на сортировку
-def process_sort_request(url, concurrency, job_id):
-    try:
-        urllib.request.urlretrieve(url, f"{job_id}.data")
-    except Exception as e:
-        with sorting_data_lock:
-            sorting_data[job_id] = {
-                "state": "error",
-                "data": []
-            }
-        return
-
-    with open(f"{job_id}.data", "r") as f:
-        numbers = [int(num) for num in f.read().strip().split()]
-
-    chunk_size = len(numbers) // concurrency
-    chunks = [numbers[i:i + chunk_size] for i in range(0, len(numbers), chunk_size)]
-
-    for chunk in chunks:
-        sorting_queue.put((chunk, job_id))
-
-
 # Функция worker для сортировки кусков данных
 def worker():
     while True:
-        chunk, job_id = sorting_queue.get()
-        result = merge_sort(chunk)
+        thread_name = threading.current_thread().name
+        print(f"Worker thread: {thread_name}")
 
-        with sorting_data_lock:
-            sorting_data[job_id] = {
-                "state": "ready",
-                "data": result
-            }
+        query_params, job_id = sorting_queue.get()
+        url = query_params.get("sort", [None])[0]
+        concurrency = int(query_params.get("concurrency", [1])[0])
 
-        result_queue.put((result, job_id))
-        sorting_queue.task_done()
-        # Удаление временного файла после завершения сортировки
-        os.remove(f"{job_id}.data")
+        if url is None:
+            with sorting_data_lock:
+                sorting_data[job_id] = {
+                    "state": "error",
+                    "data": []
+                }
+            sorting_queue.task_done()
+            continue
+
+        try:
+            with urllib.request.urlopen(url) as response:
+                data = response.read().decode("utf-8")
+                numbers = [int(num) for num in data.strip().split()]
+                chunk_size = len(numbers) // concurrency
+                chunks = [numbers[i:i + chunk_size] for i in range(0, len(numbers), chunk_size)]
+
+                result_chunks = []
+                for chunk in chunks:
+                    result_chunk = merge_sort(chunk)
+                    result_chunks.append(result_chunk)
+
+                result = reduce(merge, result_chunks)
+
+                with sorting_data_lock:
+                    sorting_data[job_id] = {
+                        "state": "ready",
+                        "data": result
+                    }
+
+                result_queue.put((result, job_id))
+        except Exception as e:
+            with sorting_data_lock:
+                sorting_data[job_id] = {
+                    "state": "error",
+                    "data": []
+                }
+        finally:
+            sorting_queue.task_done()
+
 
 # Класс HTTP-обработчика для сервера
 class SortingServerHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        thread_name = threading.current_thread().name
+        print(f"Server thread: {thread_name}")
+
+
         if self.path.startswith("/?sort="):
             query_params = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
-            url = query_params.get("sort", [None])[0]
-            concurrency = int(query_params.get("concurrency", [1])[0])
-
-            if url is None:
-                self.send_response(400)
-                self.end_headers()
-                self.wfile.write(b"Invalid parameters")
-                return
-
             job_id = str(time.time()).replace(".", "")
-            sorting_thread = threading.Thread(target=process_sort_request, args=(url, concurrency, job_id))
-            sorting_thread.start()
+            sorting_queue.put((query_params, job_id))
 
             with sorting_data_lock:
                 sorting_data[job_id] = {
                     "state": "queued",
                     "data": []
                 }
-
             self.send_response(200)
             self.end_headers()
             self.wfile.write(job_id.encode())
@@ -154,7 +160,7 @@ def start_server():
 
 if __name__ == "__main__":
     server_thread = threading.Thread(target=start_server)
-    num_worker_threads = 4
+    num_worker_threads = 2
     worker_threads = [threading.Thread(target=worker) for _ in range(num_worker_threads)]
 
     server_thread.start()
